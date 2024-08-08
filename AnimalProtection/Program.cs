d57b52daaa86dfbca40858306a33767ddd1a6f15
CaePolit <caepolit@gmail.com>
2024-08-08 10:22:03 +0200

    Ajoutez des fichiers projet.

---
+++ AnimalProtection/Program.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System.Data;
+
+
+namespace LINQMethodDemo
+
+{
+
+    public class AnimalContext : DbContext
+
+    {
+
+        public DbSet<Animal> Animals { get; set; }
+
+        public DbSet<Species> Species { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+
+        {
+
+            builder.UseSqlServer(/* Your connection string */);
+
+        }
+
+    }
+
+
+    public class Animal
+
+    {
+
+        public Guid AnimalId { get; set; }
+
+        public String Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public Species Species { get; set; }
+
+    }
+
+
+    public class Species
+
+    {
+
+        public Guid SpeciesId { get; set; }
+
+        public String Name { get; set; }
+
+    }
+
+
+    public class Program
+
+    {
+
+        public static void Main()
+
+        {
+
+            using (var context = new AnimalContext())
+
+            {
+
+                var whiteCougarSpecies = context.Species.Where((s) => s.Name == "White cougar");
+
+                IEnumerable<Animal> whiteCougars = context.Animals.Where((a) => a.Species == whiteCougarSpecies);
+
+            }
+
+        }
+
+    }
